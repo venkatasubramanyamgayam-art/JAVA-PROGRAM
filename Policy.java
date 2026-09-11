bec9bd9a0f31595d100ed7a59534a9e7b5ca33f5
venkatasubramanyamgayam-art <venkatasubramanyamgayam@gmail.com>
2026-09-11 21:45:51 +0530

    Add files via upload

---
+++ Policy.java
@@ -0,0 +1,21 @@
+package Day10;
+class Policy{
+	private int pol_no;	
+	private double cov;
+	private float pre;
+	private int dur;
+	private String nom;
+	private String hol;
+	public void setPol_no(int pol_no){this.pol_no=pol_no;}
+	public int getPol_no(return pol_no;)
+	public void Cov(double cov){this.cov=cov;}
+	public double getCov(return cov;)
+	public void setPre(float pre){this.pre=pre;}
+	public float getPre(return pre;)
+	public void setDur(int dur){this.dur=dur;}
+	public int getDur(return dur;)
+	public void setNom(String nom){this.nom=nom;}
+	public String getNom(return nom;)
+	public void setHol(String hol){this.hol=hol;}
+	public String getHol(return hol;)
+}
